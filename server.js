3f64a6b9e64978d1859302741a3ddb60ec9666cb
ChumakSV <alone.nnov@gmail.com>
2021-11-18 23:07:46 +0300

    static

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
-const PORT = 3000;
 const app = express();
 
-app.use(express.static(`${__dirname}/src`));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(PORT, () => {
-  console.log(`Мой текст в логе после запуска ${PORT}!`);
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+      })
